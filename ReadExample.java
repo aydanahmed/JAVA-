5f6a1e928991ca306a499f8cea238d9a4fba0d82
HP450\Aydan <aydan.ahmed1994@gmail.com>
2017-08-14 13:16:26 +0300

    Read Example -> BufferReader

---
+++ ReadExample.java
@@ -0,0 +1,26 @@
+package testing;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadExample {
+	
+	private static final String FILENAME = "src/testingdoc";
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new FileReader(FILENAME))) {
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
